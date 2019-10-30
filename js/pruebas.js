b4ddf02c0ffcd0276aa4331908296c680968220e
Manuel Aguirre <aguirre.manuel7@gmail.com>
2019-10-30 14:23:06 +0100

    se completan funciones de prueba

--- js/pruebas.js
+++ js/pruebas.js
@@ -10,6 +10,8 @@ function probarValidarNombre() {
         'Este campo debe tener menos de 50 caracteres',
         'Validar nombre no validó que el nombre sea menor a 50 caracteres',
     );
+
+    console.assert(validarNombre("Juan Pablo") === "", "validarNombre no validó un nombre válido")
 }
 
 function pruebaValidarCiudad() {
@@ -17,6 +19,9 @@ function pruebaValidarCiudad() {
         validarCiudad("") === "Por favor seleccione la ciudad", 
         "validarCiudad no validó que la ciudad no sea vacía",
         );
+    console.assert(
+        validarCiudad("Salta") === "", "validarCiudad() no validó ciudad válida"
+    )    
 }
 
 
@@ -35,5 +40,13 @@ function pruebaValidarCiudad() {
 
 
 
-probarValidarNombre();
-pruebaValidarCiudad();
+function probarValidarDescripcionRegalo(){
+    console.assert(validarDescripcionRegalo("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
+    === "La descripción del regalo debe contener a lo sumo 300 caracteres", 
+    "validarDescripcionRegalo validó descripción demasiado larga"
+    );
+    console.assert(validarDescripcionRegalo("") === "No hay descripción del regalo", "validarDescripcionRegalo() validó una descripción vacía"
+    );
+    console.assert(validarDescripcionRegalo("un autito Hot Wheels") === "", "validarDescripcionRegalo() no validó un regalo válido");
+};
+
